e5e1f41e2fe7333b6c89b2c7c121a1653e24cb8e
Alfarizi <arzalfarizi12@gmail.com>
2025-04-12 12:33:58 +0700

    Update Login feature and add more item

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,56 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
   container.classList.remove('right-panel-active');
 });
+
+// Toggle UI
+// const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signIn');
+// const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+  container.classList.remove("right-panel-active");
+});
+
+// Register
+const signUpForm = document.querySelector('.sign-up-container form');
+signUpForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const firstName = signUpForm.querySelector('input[placeholder="First Name"]').value;
+  const lastName = signUpForm.querySelector('input[placeholder="Last Name"]').value;
+  const email = signUpForm.querySelector('input[placeholder="Email"]').value;
+  const password = signUpForm.querySelector('input[placeholder="Password"]').value;
+
+  // localStorage
+  const userData = {
+    fullName: `${firstName} ${lastName}`,
+    email,
+    password
+  };
+
+  localStorage.setItem('user', JSON.stringify(userData));
+  alert('Register successful! You can now log in.');
+  container.classList.remove("right-panel-active");
+});
+
+// Login
+const signInForm = document.querySelector('.sign-in-container form');
+signInForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const usernameInput = signInForm.querySelector('input[placeholder="Username"]').value;
+  const passwordInput = signInForm.querySelector('input[placeholder="Password"]').value;
+
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+
+  if (storedUser && (usernameInput === storedUser.email || usernameInput === storedUser.fullName) && passwordInput === storedUser.password) {
+    // alert('Login successful!');
+    window.location.href = "dashboard.html";
+  } else {
+    alert('Login failed! Please check your credentials.');
+  }
+});
